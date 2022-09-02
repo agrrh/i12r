f2c21dd5df4a9a96a291eb127b0e092c3d745fcd
Kirill K <kovalev.kirill.a@gmail.com>
2022-09-02 23:30:06 +0300

    feat: list files

--- cli/main.py
+++ cli/main.py
@@ -1,8 +1,58 @@
 # TODO Tool to detect issues
 
-if __name__ == "__main__":
-    pass
+import fire
+import sys
+import os
+import glob
 
-    # TODO Scan directory
-    # TODO Provide sane defaults
-    # TODO Filter files by include/exclude patterns
+
+from rich import print as rprint
+
+
+def eprint(s: str):
+    rprint(s, file=sys.stderr)
+
+
+def get_files(path: str):
+    for root, dirs, files in os.walk(path, followlinks=False):
+        for file in files:
+            yield os.path.join(root, file)
+
+
+def get_excludes(exclude: list):
+    for pattern in exclude:
+        for path in glob.glob(pattern, recursive=True):
+            yield path
+
+
+def get_includes(include: list):
+    for pattern in include:
+        for path in glob.glob(pattern, recursive=True):
+            yield path
+
+
+def cli_entrypoint(debug: bool = False, path: str = "./", include: list = [], exclude: list = ["./.git/**"]):
+    eprint(f"# Working with root: {path}")
+    eprint(f"# Include only: {include}")
+    eprint(f"# Exclude from: {exclude}")
+
+    files = list(get_files(path))
+
+    if include:
+        includes = list(get_includes(include))
+        files = set(files).intersection(set(includes))
+
+    excludes = list(get_excludes(exclude))
+    files = [file for file in files if file not in excludes]
+
+    eprint(f"# Files found: {files}")
+
+    # TODO Call for i12r library to get issues
+    # TODO Print issues found
+
+
+if __name__ == "__main__":
+    # TODO Get include/exclude from global config
+    # TODO Get include/exclude from in-repository config
+
+    fire.Fire(cli_entrypoint)
