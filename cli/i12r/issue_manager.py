a1ca75bd70a508f46433c9184f564a1d6a779145
Kirill K <kovalev.kirill.a@gmail.com>
2022-09-04 16:21:05 +0300

    chore: bump todos

--- cli/i12r/issue_manager.py
+++ cli/i12r/issue_manager.py
@@ -24,7 +24,7 @@ class IssueManager:
 
             for match in matches:
                 # TODO Handle errors (e.g. wrong types)
-                # TODO Strip trailing newlines
+                # TODO Strip trailing whitespaces and newlines
                 # TODO Remove common identation for strings
 
                 pos_start = match.start()
